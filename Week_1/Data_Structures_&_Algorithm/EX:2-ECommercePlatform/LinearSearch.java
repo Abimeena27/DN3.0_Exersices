04c1f9c20cd7258493da3fc1f76f5d17a9d0f959
Abimeena27 <111678732+Abimeena27@users.noreply.github.com>
2024-07-31 13:44:31 +0530

    Create LinearSearch.java

---
+++ Week_1/Data_Structures_&_Algorithm/EX:2-ECommercePlatform/LinearSearch.java
@@ -0,0 +1,12 @@
+public class SearchAlgorithms {
+
+    // Linear search
+    public static int linearSearch(Product[] products, String targetProductName) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i].getProductName().equals(targetProductName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
